48946bca1c8d6f3112081d963dca9078f22d373c
oladayo Adekoya <adekoyadayo2010@gmail.com>
2024-02-04 15:24:25 +0100

    include navbar component

--- vision_front-end/og-vision/src/pages/Dashboard.jsx
+++ vision_front-end/og-vision/src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 const Dashboard = () => {
-  return <h1 className="text-black">Dashboard</h1>;
+  return <h1>Dashboard</h1>;
 };
 
 export default Dashboard;
--- vision_front-end/og-vision/src/pages/Screenshots.jsx
+++ vision_front-end/og-vision/src/pages/Screenshots.jsx
@@ -1,5 +1,5 @@
 const Screenshots = () => {
-  return <h1 className="text-black">Screenshots</h1>;
+  return <h1>Screenshots</h1>;
 };
 
 export default Screenshots;
