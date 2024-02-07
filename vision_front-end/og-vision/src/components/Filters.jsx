3e2c90811786f9c00e28681ddb87c7a6f6070c00
oladayo Adekoya <adekoyadayo2010@gmail.com>
2024-02-07 14:59:01 +0100

    update filters

--- vision_front-end/og-vision/src/components/Filters.jsx
+++ vision_front-end/og-vision/src/components/Filters.jsx
@@ -1,13 +1,89 @@
 const Filters = () => {
   return (
-    <div className="align-element mt-12 mb-6 bg-lime-300">
-      <div className="flex justify-between">
-        <div className="bg-red-200 border-2 border-indigo-500/75 w-4">
-          filtersfiltersfiltersfiltersfiltersfiltersfiltersfiltersfilters
+    <div className="align-element mt-12 mb-6 bg-lime-30">
+      <div className="flex justify-between lg:gap-x-5 gap-x-3">
+        <div className="w-1/4">
+          <div className="relative mt-2 rounded-md shadow-sm">
+            <input
+              type="date"
+              name="date"
+              id="date"
+              className="block w-full h-full rounded-md border-0 py-1.5 pl-5 pr-2 text-gray-600 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
         </div>
-        <div className="bg-red-200 border-2 border-indigo-500/75">filters</div>
-        <div className="bg-red-200 border-2 border-indigo-500/75">filters</div>
-        <div className="bg-red-200 border-2 border-indigo-500/75">filters</div>
+        <div className="w-1/4 grid place-content-center">
+          <select
+            id="user"
+            name="user"
+            className="w-full bg-transparent border-2 border-sky-500 rounded-md"
+          >
+            <option>Select user</option>
+            <option>CAD</option>
+            <option>EUR</option>
+          </select>
+        </div>
+
+        <div className="w-1/4">
+          <div>
+            <div className="relative mt-2 rounded-md shadow-sm">
+              <input
+                type="text"
+                name="search"
+                id="search"
+                className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="Search user"
+              />
+              <div className="absolute inset-y-0 right-0 flex items-center">
+                <svg
+                  className="w-5 h-5 mr-2"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M18.5 19.5L20 21M4 21C4 17.134 7.13401 14 11 14M19 17.5C19 18.8807 17.8807 20 16.5 20C15.1193 20 14 18.8807 14 17.5C14 16.1193 15.1193 15 16.5 15C17.8807 15 19 16.1193 19 17.5ZM15 7C15 9.20914 13.2091 11 11 11C8.79086 11 7 9.20914 7 7C7 4.79086 8.79086 3 11 3C13.2091 3 15 4.79086 15 7Z"
+                    stroke="#000000"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-1/4">
+          <div>
+            <div className="relative mt-2 rounded-md shadow-sm">
+              <input
+                type="time"
+                name="time"
+                id="time"
+                className="block w-full rounded-md border-0 py-1.5 pl-5 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+              <div className="absolute inset-y-0 right-0 flex items-center">
+                <svg
+                  className="w-5 h-5 mr-2"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g id="Menu / Close_MD">
+                    <path
+                      id="Vector"
+                      d="M18 18L12 12M12 12L6 6M12 12L18 6M12 12L6 18"
+                      stroke="#000000"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </g>
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
